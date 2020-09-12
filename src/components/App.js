8b92a6b7a69c6440da6eaffe5e9b57828c9e28c0
Greg Mrowczynski <greg19850@gmail.com>
2020-09-12 20:50:49 +0100

    update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -36,21 +36,6 @@ class App extends Component {
               <AllTransactions />
             </Route>
           </main>
-          <footer>
-            <div>
-              Icons made by{" "}
-              <a
-                href="https://www.flaticon.com/authors/freepik"
-                title="Freepik"
-              >
-                Freepik
-              </a>{" "}
-              from{" "}
-              <a href="https://www.flaticon.com/" title="Flaticon">
-                www.flaticon.com
-              </a>
-            </div>
-          </footer>
         </div>
       </Router>
     );
