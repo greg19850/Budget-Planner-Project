fa2685375a83f8210621f8eb6e0da858a226ae72
Greg Mrowczynski <greg19850@gmail.com>
2020-09-26 19:33:44 +0100

    create expenseForm, connect with state

---
+++ src/components/expenseForm/ExpenseForm.js
@@ -0,0 +1,118 @@
+import React, { Component } from "react";
+import "./ExpenseForm.css";
+import { connect } from "react-redux";
+import { addExpense } from "../../actions/accountSummaryActions";
+import PropTypes from "prop-types";
+
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+
+class ExpenseForm extends Component {
+  state = {
+    amount: "",
+    category: "Clothes",
+    date: new Date(),
+    description: "",
+  };
+
+  handleChange = (e) => this.setState({ [e.target.name]: e.target.value });
+
+  handleDateChange = (date) => {
+    this.setState({
+      date,
+    });
+  };
+
+  handleFormSubmit = (e) => {
+    e.preventDefault();
+    this.props.addExpense(this.state.amount * 1);
+
+    this.setState({
+      amount: "",
+      category: "Clothes",
+      date: new Date(),
+      description: "",
+    });
+  };
+
+  handleFormFieldsReset = (e) => {
+    e.preventDefault();
+    this.setState({
+      amount: "",
+      category: "Clothes",
+      date: new Date(),
+      description: "",
+    });
+  };
+
+  render() {
+    return (
+      <form>
+        <label>
+          Amount:
+          <input
+            type="number"
+            name="amount"
+            id="amount"
+            value={this.state.amount || ""}
+            onChange={this.handleChange}
+          />
+        </label>
+        <label>
+          Category:
+          <select
+            id="category"
+            name="category"
+            value={this.state.category}
+            onChange={this.handleChange}
+          >
+            <option value="Clothes">Salary</option>
+            <option value="Education">Salary</option>
+            <option value="Eating Out">Loan</option>
+            <option value="Entertainment">Other</option>
+            <option value="Food">Other</option>
+            <option value="Fuel">Other</option>
+            <option value="Gift">Other</option>
+            <option value="Health">Other</option>
+            <option value="Holiday">Other</option>
+            <option value="Shopping">Other</option>
+            <option value="Travel">Other</option>
+            <option value="Other">Other</option>
+          </select>
+        </label>
+        <label>
+          Date and Time:
+          <DatePicker
+            selected={this.state.date}
+            name="date"
+            onChange={this.handleDateChange}
+            timeInputLabel="Time:"
+            dateFormat="dd/MM/yyyy h:mm aa"
+            showTimeInput
+          />
+        </label>
+        <label>
+          Description:
+          <textarea
+            id="description"
+            name="description"
+            value={this.state.description}
+            onChange={this.handleChange}
+          ></textarea>
+        </label>
+        <section className="buttons">
+          <button onClick={this.handleFormSubmit}>Save</button>
+          <button onClick={this.handleFormFieldsReset}>Reset</button>
+        </section>
+      </form>
+    );
+  }
+}
+
+ExpenseForm.propTypes = {
+  addExpense: PropTypes.func.isRequired,
+};
+
+const mapDispatchToProps = { addExpense };
+
+export default connect(null, mapDispatchToProps)(ExpenseForm);
--- src/reducers/accountSummaryReducer.js
+++ src/reducers/accountSummaryReducer.js
@@ -8,6 +8,8 @@ const accountSummaryReducer = (state = initialState, action) => {
   switch (action.type) {
     case "ADD_INCOME":
       return { ...state, incomeSummary: state.incomeSummary + action.amount };
+    case "ADD_EXPENSE":
+      return { ...state, incomeSummary: state.expenseSummary + action.amount };
     default:
       return state;
   }
