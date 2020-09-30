b0dcb3216996be412eb7b1c48dd381c7f0720442
Greg Mrowczynski <greg19850@gmail.com>
2020-09-30 20:16:23 +0100

    create AccountSummarySelectors.js, update Balance.js

--- src/components/balance/Balance.js
+++ src/components/balance/Balance.js
@@ -1,18 +1,18 @@
 import React from "react";
 import "./Balance.css";
 import balance from "../../img/balance.svg";
-
 import { useSelector } from "react-redux";
+import { getBalance } from "../../selectors/AccountSummarySelectors";
 
 const Balance = () => {
-  const income = useSelector((state) => state.accountSummary.incomeSummary);
-  const expense = useSelector((state) => state.accountSummary.expenseSummary);
+  const budgetBalance = useSelector(getBalance);
   return (
     <div>
       <img src={balance} alt="balance Logo" />
       <br />
       Balance
-      <br />${income - expense}
+      <br />
+      {budgetBalance}
     </div>
   );
 };
