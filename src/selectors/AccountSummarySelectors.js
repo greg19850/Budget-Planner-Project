804ac9a9f39561a2d22efcb910d3ab34d9ded208
Greg Mrowczynski <greg19850@gmail.com>
2020-10-01 18:11:24 +0100

    add fetIncome and getExpense selectors to AccountSummarySelectors.js

--- src/selectors/AccountSummarySelectors.js
+++ src/selectors/AccountSummarySelectors.js
@@ -3,3 +3,13 @@ export const getBalance = (state) => {
   const expense = state.accountSummary.expenseSummary;
   return `$${income - expense}`;
 };
+
+export const getIncome = (state) => {
+  const income = state.accountSummary.incomeSummary;
+  return income;
+};
+
+export const getExpense = (state) => {
+  const expense = state.accountSummary.expenseSummary;
+  return expense;
+};
